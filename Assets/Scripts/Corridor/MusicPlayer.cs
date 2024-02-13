0168208780b15cff09d24682d7a9f61eee2986d3
Mack Pearson <chopov1@gmail.com>
2024-02-12 20:29:02 -0600

    Added Post Process Clue to Maze

--- Assets/Scripts/Corridor/MusicPlayer.cs
+++ Assets/Scripts/Corridor/MusicPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [RequireComponent(typeof(AudioSource))]
 public class MusicPlayer : MonoBehaviour
@@ -9,6 +10,9 @@ public class MusicPlayer : MonoBehaviour
     float initDis;
     [SerializeField]
     float lowestPitch;
+
+    [SerializeField]
+    Volume postProcess;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +25,12 @@ public class MusicPlayer : MonoBehaviour
     {
         float dis = Vector3.Distance(transform.position, MazePlayerController.Instance.transform.position);
         float ratio = dis / initDis;
+        float weight = (1 - ratio);
+        Debug.Log(weight);
+        Mathf.Clamp(weight, 0, 1);
         float pitch = lowestPitch + (1-lowestPitch)* (1 - ratio) + 0.1f;
         pitch = Mathf.Clamp(pitch, 0.5f, 1.0f);
         musicSource.pitch = pitch;
+        postProcess.weight = weight;
     }
 }
